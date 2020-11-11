4855ceede6a16e8d8f1a33b95006db4738d912c6
Michael S <mi851sch@htwg-konstanz.de>
2020-11-11 17:21:40 +0100

    Aufgabe 2

---
+++ Aufgabe2/KalenderTest.java
@@ -0,0 +1,66 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class KalenderTest {
+	Kalender kalender;
+	
+	@BeforeAll
+	void setUp() throws Exception {
+		kalender = new Kalender();
+	}
+
+	@AfterAll
+	void tearDown() throws Exception {
+		kalender = null;
+	}
+
+	@Test
+	@DisplayName("Ein Tag im Januar")
+	void testTagimJanuar() {
+		int tag = 5;
+		int monat = 1;
+		
+		boolean actualResult = kalender.tagGültig(tag, monat);
+		
+		assertTrue(actualResult);
+	}
+	
+	@Test
+	@DisplayName("Ein Tag im Februar")
+	void testTagimFebruar() {
+		int tag = 28;
+		int monat = 2;
+		
+		boolean actualResult = kalender.tagGültig(tag, monat);
+		
+		assertTrue(actualResult);
+	}
+	
+	@Test
+	@DisplayName("Ein Tag im Juni")
+	void testTagimJuni() {
+		int tag = 5;
+		int monat = 6;
+		
+		boolean actualResult = kalender.tagGültig(tag, monat);
+		
+		assertTrue(!actualResult);
+	}
+	
+	@Test
+	@DisplayName("Ungültige Eingabeparameter")
+	void testUngueltigesDatum() {
+		int tag = 50;
+		int monat = 100;
+		
+		boolean actualResult = kalender.tagGültig(tag, monat);
+		
+		assertTrue(!actualResult);
+	}
+}
